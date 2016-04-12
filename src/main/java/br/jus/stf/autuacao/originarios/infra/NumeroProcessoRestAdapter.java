7844e1b202ab9653fe38e57d362de7002030a5cf
Rodrigo Barreiros <barreiros.rodrigo@gmail.com>
2016-04-11 22:29:54 -0300

    Script de construção e merge com a versão flyway

--- src/main/java/br/jus/stf/autuacao/originarios/infra/NumeroProcessoRestAdapter.java
+++ src/main/java/br/jus/stf/autuacao/originarios/infra/NumeroProcessoRestAdapter.java
@@ -2,6 +2,8 @@ package br.jus.stf.autuacao.originarios.infra;
 
 import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -18,9 +20,14 @@ import br.jus.stf.core.shared.processo.Identificacao;
 @Component
 public class NumeroProcessoRestAdapter implements NumeroProcessoAdapter {
 	
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    
 	@Override
 	public Identificacao novoNumeroProcesso(String classe) {
-		URI uri = UriComponentsBuilder.fromUriString("http://localhost:8081/api").path("/identificadores").queryParam("categoria", classe).build().toUri();
+		URI servicesUri = discoveryClient.getInstances("services").get(0).getUri();
+		
+		URI uri = UriComponentsBuilder.fromUri(servicesUri).path("/api/identificadores").queryParam("categoria", classe).build().toUri();
 		
 		IdentificacaoDto identificacao = new RestTemplate().getForObject(uri, IdentificacaoDto.class);
     	
