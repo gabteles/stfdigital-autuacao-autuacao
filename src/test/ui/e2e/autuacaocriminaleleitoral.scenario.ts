9f9e1ad32c7a2c01f629ba7d2e090b32c5c8cea8
viniciusksilva <viniciusksilva@gmail.com>
2016-06-08 17:21:05 -0300

    Teste e2e criado para autuacao criminal eleitoral

--- src/test/ui/e2e/autuacaocriminaleleitoral.scenario.ts
+++ src/test/ui/e2e/autuacaocriminaleleitoral.scenario.ts
@@ -1,28 +1,28 @@
 import {LoginPage} from "./pages/login.page";
 import {PrincipalPage}  from "./pages/principal.page";
-import {AnalisePage} from "./pages/analise.page";
+import {AutuacaoRePage} from "./pages/autuacaoRe.page";
 
 describe('Autuação de Processos Recursais criminal e eleitoral', () => {	
 	
     var loginPage: LoginPage = new LoginPage();
     var principalPage: PrincipalPage = new PrincipalPage();
-	var analisePage : AnalisePage = new AnalisePage();
+	var autuacaoRe : AutuacaoRePage = new AutuacaoRePage();
                 
     it ('Deveria logar na tela', () => {
         loginPage.open();
         loginPage.login('aaa', '123');
     });
     
-    it ('Deveria acessar a pagina de análise de pressupostos', () => {
+    it ('Deveria acessar a pagina de autuacao criminais e eleitorais', () => {
         principalPage.iniciarProcesso();
         principalPage.iniciarAutuacaoCriminalEleitoral();
     });
     
     it('Deveria analisar os pressupostos do processo recursal informando como inapto ', () => {
-    	analisePage.classificarInaptidao();
-    	analisePage.selecionarMotivos();
-    	analisePage.preencherObservacao();
-    	analisePage.registrarAnalise();
+    	autuacaoRe.cadastrarPoloAtivo('Maria');
+    	autuacaoRe.cadastrarPoloPassivo('Telmo');
+    	autuacaoRe.selecionarAssuntos('DIREITO');
+    	autuacaoRe.registrarAutuacaoCriminalEleitoral();
     });
     
 });
