7173a80df8287484f79ed4d6ebb71ab10c1c6fb5
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-06-15 20:46:23 -0300

    Adequando o projeto ui para usar bundle.

--- src/main/ui/bundle.config.js
+++ src/main/ui/bundle.config.js
@@ -14,38 +14,16 @@ var libraryTypeScript = path.join(conf.paths.src, 'typings/main/**/*.d.ts');
 
 module.exports = {
   bundle: {
-    'peticoes-fisicas': {
-      scripts: [path.join(conf.paths.app, 'peticoes-fisicas.ts'),
-                path.join(conf.paths.app, 'peticoes-fisicas/**/*.ts'), libraryTypeScript],
-      options: {
-    	  rev: false,
-    	  transforms: {
-              scripts: lazypipe()
-              	.pipe(typescript, createTsProject())
-              	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'recebimento/'})
-              	.pipe(ngAnnotate)
-              	.pipe(embedTemplates, {
-              		skipErrors: true, 
-              		minimize: {
-              			empty : true,
-              			spare : true,
-              			quotes: true
-              		}
-              	})
-          }
-      }
-    },
-    'preautuacao': {
-      scripts: [path.join(conf.paths.app, 'preautuacao.ts'),
-                path.join(conf.paths.app, 'preautuacao/**/*.ts'),
-                path.join(conf.paths.app, 'services.ts'),
+    'analise-pressupostos': {
+      scripts: [path.join(conf.paths.app, 'analise-pressupostos.ts'),
+                path.join(conf.paths.app, 'recursal/pressuposto/analise/**/*.ts'),
                 path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
       options: {
     	  rev: false,
     	  transforms: {
               scripts: lazypipe()
               	.pipe(typescript, createTsProject())
-              	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'recebimento/'})
+              	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'autuacao/'})
               	.pipe(ngAnnotate)
               	.pipe(embedTemplates, {
               		skipErrors: true, 
@@ -58,94 +36,138 @@ module.exports = {
           }
       }
     },
-    'preautuacao-recursal': {
-        scripts: [path.join(conf.paths.app, 'preautuacao-recursal.ts'),
-                  path.join(conf.paths.app, 'preautuacao/recursal/**/*.ts'),
-                  path.join(conf.paths.app, 'services.ts'),
-                  path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
-        options: {
-      	  rev: false,
-      	  transforms: {
-                scripts: lazypipe()
-                	.pipe(typescript, createTsProject())
-                	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'recebimento/'})
-                	.pipe(ngAnnotate)
-                	.pipe(embedTemplates, {
-                		skipErrors: true, 
-                		minimize: {
-                			empty : true,
-                			spare : true,
-                			quotes: true
-                		}
-                	})
-            }
-        }
-      },
-      'services': {
-          scripts: [path.join(conf.paths.app, 'services.ts'),
-                    path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
-          options: {
-        	  rev: false,
-        	  transforms: {
-                  scripts: lazypipe()
-                  	.pipe(typescript, createTsProject())
-                  	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'recebimento/'})
-                  	.pipe(ngAnnotate)
-                  	.pipe(embedTemplates, {
-                  		skipErrors: true, 
-                  		minimize: {
-                  			empty : true,
-                  			spare : true,
-                  			quotes: true
-                  		}
-                  	})
-              }
+    'analise-repercussao-geral': {
+      scripts: [path.join(conf.paths.app, 'analise-repercussao-geral.ts'),
+                path.join(conf.paths.app, 'recursal/repercussao/analise/**/*.ts'),
+                path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
+      options: {
+    	  rev: false,
+    	  transforms: {
+              scripts: lazypipe()
+              	.pipe(typescript, createTsProject())
+              	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'autuacao/'})
+              	.pipe(ngAnnotate)
+              	.pipe(embedTemplates, {
+              		skipErrors: true, 
+              		minimize: {
+              			empty : true,
+              			spare : true,
+              			quotes: true
+              		}
+              	})
           }
-        },
-        'preparar-oficio-devolucao': {
-            scripts: [path.join(conf.paths.app, 'preparar-oficio-devolucao.ts'),
-                      path.join(conf.paths.app, 'preparar-oficio-devolucao/**/*.ts'), libraryTypeScript],
-            options: {
-          	  rev: false,
-          	  transforms: {
-                    scripts: lazypipe()
-                    	.pipe(typescript, createTsProject())
-                    	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'recebimento/'})
-                    	.pipe(ngAnnotate)
-                    	.pipe(embedTemplates, {
-                    		skipErrors: true, 
-                    		minimize: {
-                    			empty : true,
-                    			spare : true,
-                    			quotes: true
-                    		}
-                    	})
-                }
-            }
-          },
-          'devolucao-assinatura': {
-              scripts: [path.join(conf.paths.app, 'devolucao-assinatura.ts'),
-                        path.join(conf.paths.app, 'devolucao-assinatura/**/*.ts'),
-                        path.join(conf.paths.app, 'services.ts'),
-                        path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
-              options: {
-            	  rev: false,
-            	  transforms: {
-                      scripts: lazypipe()
-                      	.pipe(typescript, createTsProject())
-                      	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'recebimento/'})
-                      	.pipe(ngAnnotate)
-                      	.pipe(embedTemplates, {
-                      		skipErrors: true, 
-                      		minimize: {
-                      			empty : true,
-                      			spare : true,
-                      			quotes: true
-                      		}
-                      	})
-                  }
-              }
-            }
+      }
+    },
+    'autuacao': {
+      scripts: [path.join(conf.paths.app, 'autuacao.ts'),
+                path.join(conf.paths.app, 'autuacao/**/*.ts'),
+                path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
+      options: {
+    	  rev: false,
+    	  transforms: {
+              scripts: lazypipe()
+              	.pipe(typescript, createTsProject())
+              	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'autuacao/'})
+              	.pipe(ngAnnotate)
+              	.pipe(embedTemplates, {
+              		skipErrors: true, 
+              		minimize: {
+              			empty : true,
+              			spare : true,
+              			quotes: true
+              		}
+              	})
+          }
+      }
+    },
+    'autuacao-criminal': {
+      scripts: [path.join(conf.paths.app, 'autuacao-criminal.ts'),
+                path.join(conf.paths.app, 'recursal/autuacao/criminal/**/*.ts'),
+                path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
+      options: {
+    	  rev: false,
+    	  transforms: {
+              scripts: lazypipe()
+              	.pipe(typescript, createTsProject())
+              	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'autuacao/'})
+              	.pipe(ngAnnotate)
+              	.pipe(embedTemplates, {
+              		skipErrors: true, 
+              		minimize: {
+              			empty : true,
+              			spare : true,
+              			quotes: true
+              		}
+              	})
+          }
+      }
+    },
+    'autuacao-recursal': {
+      scripts: [path.join(conf.paths.app, 'autuacao-recursal.ts'),
+                path.join(conf.paths.app, 'recursal/autuacao/recursal/**/*.ts'),
+                path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
+      options: {
+    	  rev: false,
+    	  transforms: {
+              scripts: lazypipe()
+              	.pipe(typescript, createTsProject())
+              	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'autuacao/'})
+              	.pipe(ngAnnotate)
+              	.pipe(embedTemplates, {
+              		skipErrors: true, 
+              		minimize: {
+              			empty : true,
+              			spare : true,
+              			quotes: true
+              		}
+              	})
+          }
+      }
+    },
+    'revisao-analise-pressupostos': {
+      scripts: [path.join(conf.paths.app, 'revisao-analise-pressupostos.ts'),
+                path.join(conf.paths.app, 'recursal/pressuposto/revisao/**/*.ts'),
+                path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
+      options: {
+    	  rev: false,
+    	  transforms: {
+              scripts: lazypipe()
+              	.pipe(typescript, createTsProject())
+              	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'autuacao/'})
+              	.pipe(ngAnnotate)
+              	.pipe(embedTemplates, {
+              		skipErrors: true, 
+              		minimize: {
+              			empty : true,
+              			spare : true,
+              			quotes: true
+              		}
+              	})
+          }
+      }
+    },
+    'revisao-repercussao-geral': {
+      scripts: [path.join(conf.paths.app, 'revisao-repercussao-geral.ts'),
+                path.join(conf.paths.app, 'recursal/repercussao/revisao/**/*.ts'),
+                path.join(conf.paths.app, 'services/**/*.ts'), libraryTypeScript],
+      options: {
+    	  rev: false,
+    	  transforms: {
+              scripts: lazypipe()
+              	.pipe(typescript, createTsProject())
+              	.pipe(moduleNameInjector, {rootDir: 'src/main/ui/app/', prefix: 'autuacao/'})
+              	.pipe(ngAnnotate)
+              	.pipe(embedTemplates, {
+              		skipErrors: true, 
+              		minimize: {
+              			empty : true,
+              			spare : true,
+              			quotes: true
+              		}
+              	})
+          }
+      }
+    }
   },
   copy: [{
 	  src : path.join(conf.paths.app, '**/*.json'),
