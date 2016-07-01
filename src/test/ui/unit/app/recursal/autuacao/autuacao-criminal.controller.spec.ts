3a9c9b0321a0d1e0371ce2170ea7f25d3f72c447
viniciusksilva <viniciusksilva@gmail.com>
2016-07-01 16:25:16 -0300

    ISSUE 875 tela alterada para a validaçao dos campos e inserção do fluxo de rejeicao do processo na autuacao

---
+++ src/test/ui/unit/app/recursal/autuacao/autuacao-criminal.controller.spec.ts
@@ -0,0 +1,7 @@
+import {AutuacaoCriminalController} from "autuacao/recursal/autuacao/criminal/autuacao-criminal.controller";
+
+describe('Teste do controlador autuacao-criminal.controller', () => {
+	let controller : AutuacaoCriminalController;
+	var scope;
+	
+});
