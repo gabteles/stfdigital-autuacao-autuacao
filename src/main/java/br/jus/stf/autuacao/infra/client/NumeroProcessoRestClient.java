f3651c5b80332e17e872e25b784ea6e3a86a4cf7
Rodrigo Barreiros <barreiros.rodrigo@gmail.com>
2016-07-29 18:56:32 -0300

    Viabilizando acesso seguro via Rest a API protegida via Oauth Client Credentials

---
+++ src/main/java/br/jus/stf/autuacao/infra/client/NumeroProcessoRestClient.java
@@ -0,0 +1,25 @@
+package br.jus.stf.autuacao.infra.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Lucas Rodrigues
+ * 
+ * @since 1.0.0
+ * @since 12.07.2016
+ */
+@FunctionalInterface
+@FeignClient(name = "services")
+public interface NumeroProcessoRestClient {
+
+	/**
+	 * @param categoria
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET, value = "/api/identificadores")
+	Long identificador(@RequestParam(value = "categoria") String categoria);
+	
+}
