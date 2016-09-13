97cebd9f06c3fe1d465e88eb522b499727395569
Rafael.Alencar <rafaelesdras@gmail.com>
2016-09-13 16:01:19 -0300

    Ajustes na tabela de eventos.

--- src/main/resources/db/migration/V1__Versao_inicial.sql
+++ src/main/resources/db/migration/V1__Versao_inicial.sql
@@ -10,3 +10,5 @@ alter table autuacao.processo add constraint ck_proc_tip_status check (tip_statu
 create table autuacao.evento (seq_evento number not null, nom_evento varchar2(100) not null, dat_criacao date not null, bin_detalhe clob not null, tip_status smallint not null, constraint pk_evento primary key (seq_evento));
 
 create table autuacao.processo_evento (seq_processo number not null, seq_evento number not null, constraint pk_processo_evento primary key (seq_processo, seq_evento));
+alter table autuacao.processo_evento add constraint fk_processo_prev foreign key (seq_processo) references autuacao.processo(seq_processo);
+alter table autuacao.processo_evento add constraint fk_evento_prev foreign key (seq_evento) references autuacao.evento(seq_evento);
